5543b76da09fbf3da90ab5bb417d3d4879c24a22
Saikiran <saikiran3000.18@gmail.com>
2025-01-06 10:56:05 +0530

    bingo

--- Test2.java
+++ Test2.java
@@ -1,7 +1,7 @@
 public class Test2 {
     public void test3(){
-        for(int i=0; i<5; i++){
-            System.out.println(i);
+        for(int i=0; i<10; i++){
+            System.out.println(i+1);
         }
     }
     public void test2(){
